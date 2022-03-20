3874ee318892155327243fb48beaea8c6d608573
JamesXiong666 <sijianxiong123@gmail.com>
2022-03-20 11:38:21 +0800

    1

---
+++ Decision Tree.py
@@ -0,0 +1,82 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import graphviz
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from collections import Counter
+from sklearn import preprocessing
+import math
+from math import log
+import pprint
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import export_graphviz
+from sklearn import tree
+
+
+# data
+def create_data():
+    iris = load_iris()
+    df = pd.DataFrame(iris.data, columns=iris.feature_names)
+    df['label'] = iris.target
+    df.columns = [
+        'sepal length', 'sepal width', 'petal length', 'petal width', 'label'
+    ]
+    data = np.array(df.iloc[:100, [0, 1, -1]])
+    # print(data)
+    return data[:, :2], data[:, -1]
+
+
+X, y = create_data()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+clf = DecisionTreeClassifier()
+clf.fit(X_train, y_train,)
+clf.score(X_test, y_test)
+tree_pic = export_graphviz(clf, out_file="mytree.pdf")
+
+with open('mytree.pdf') as f:
+    dot_graph = f.read()
+
+graphviz.Source(dot_graph)
+
+features = ["年龄", "有工作", "有自己的房子", "信贷情况"]
+X_train = pd.DataFrame([
+    ["青年", "否", "否", "一般"],
+    ["青年", "否", "否", "好"],
+    ["青年", "是", "否", "好"],
+    ["青年", "是", "是", "一般"],
+    ["青年", "否", "否", "一般"],
+    ["中年", "否", "否", "一般"],
+    ["中年", "否", "否", "好"],
+    ["中年", "是", "是", "好"],
+    ["中年", "否", "是", "非常好"],
+    ["中年", "否", "是", "非常好"],
+    ["老年", "否", "是", "非常好"],
+    ["老年", "否", "是", "好"],
+    ["老年", "是", "否", "好"],
+    ["老年", "是", "否", "非常好"],
+    ["老年", "否", "否", "一般"]
+])
+y_train = pd.DataFrame(["否", "否", "是", "是", "否",
+                        "否", "否", "是", "是", "是",
+                        "是", "是", "是", "是", "否"])
+# 数据预处理
+le_x = preprocessing.LabelEncoder()
+le_x.fit(np.unique(X_train))
+X_train = X_train.apply(le_x.transform)
+le_y = preprocessing.LabelEncoder()
+le_y.fit(np.unique(y_train))
+y_train = y_train.apply(le_y.transform)
+# 调用sklearn.DT建立训练模型
+model_tree = DecisionTreeClassifier()
+model_tree.fit(X_train, y_train)
+
+# 可视化
+dot_data = tree.export_graphviz(model_tree, out_file=None,
+                                    feature_names=features,
+                                    class_names=[str(k) for k in np.unique(y_train)],
+                                    filled=True, rounded=True,
+                                    special_characters=True)
+graph = graphviz.Source(dot_data)
+graph.view()
